de110f74321a99e9755526f5d104e948dc84184b
Ailten <faouzi26b@live.fr>
2025-09-29 20:38:08 +0200

    advence, issue on texuture, need print debug

---
+++ classFolder/Layer.py
@@ -0,0 +1,57 @@
+import pyray
+import pathlib
+
+# Layer, mimic a layer (sprite) with his hown movement.
+class Layer():
+    defaultSizeX = 400
+    defaultSizeY = 600
+    pathMainFolder = pathlib.Path(__file__).parent.parent.resolve()
+    defaultSource = pyray.Rectangle(0, 0, defaultSizeX, defaultSizeY)
+
+    # constructor.
+    def __init__(self, nameFile: str, isActive=True, typeLayer=None):
+        
+        # get path.
+        pathFilePng = f"{Layer.pathMainFolder}/spryte/{nameFile}.png"
+
+        # load texture.
+        self.texture = pyray.load_texture(pathFilePng)
+
+        # params.
+        self.isActive = isActive
+        self.typeLayer = typeLayer
+
+
+    # function to draw the layer.
+    def draw(self):
+
+        if not self.isActive:
+            return
+
+        rectDest = pyray.Rectangle(
+            0, 
+            0, 
+            Layer.defaultSizeX, 
+            Layer.defaultSizeY
+        )
+        origin = pyray.Vector2(
+            Layer.defaultSizeX / 2,
+            Layer.defaultSizeY / 2
+        )
+        rotation = 0
+
+        # Doc : https://electronstudio.github.io/raylib-python-cffi/pyray.html#pyray.draw_texture_pro
+        pyray.draw_texture_pro(
+            texture=self.texture,
+            source=Layer.defaultSource,
+            dest=rectDest,
+            origin=origin,
+            rotation=rotation,
+            tint=pyray.WHITE
+        )
+    
+
+    # unload all alloc for layer.
+    def destroy(self):
+
+        pyray.unload_texture(self.texture)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyray # python3 -m pip install raylib==5.5.0.3
 import asyncio # to call async func from a sync.
+from classFolder.Layer import Layer
 
 # Doc : https://electronstudio.github.io/raylib-python-cffi/pyray.html#pyray.Color
 
@@ -12,18 +13,48 @@ async def main():
     # make class Layers (like Entity).
 
     # build a window.
-    pyray.init_window(500, 500, "pngtuberAilt_V3")
+    pyray.init_window(Layer.defaultSizeX, Layer.defaultSizeY, "pngtuberAilt_V3")
     backgroundColor = pyray.Color(0, 0, 255, 255)
+
+    layers = [
+        Layer("axoHat2", isActive=False),
+        Layer("couetteLeft"),
+        Layer("couetteRight"),
+        Layer("couetteUpLeft"),
+        Layer("tronc"),
+        Layer("maidTronc", isActive=False),
+        Layer("face"),
+        Layer("couetteUpRight"),
+        Layer("mouth"),
+        Layer("mouthOpenMid"),
+        Layer("mouthOpen"),
+        Layer("eyes"),
+        Layer("eyesClose"),
+        Layer("maidHat", isActive=False),
+        Layer("hairTop"),
+        Layer("eyesBorder"),
+        Layer("axoHat1", isActive=False)
+    ]
 
     # loop update.
     while not pyray.window_should_close():
 
         pyray.begin_drawing()
         pyray.clear_background(backgroundColor)
-        pyray.draw_text("Hello world", 190, 200, 20, pyray.BLACK)
+        
+        # draw layers.
+        for l in layers:
+            l.draw()
+
         pyray.end_drawing()
 
+        # wait update (frame rate).
         await asyncio.sleep(0)
+
+
+    # free layers.
+    for l in layers:
+        l.destroy()
 
     # close window.
     pyray.close_window()
